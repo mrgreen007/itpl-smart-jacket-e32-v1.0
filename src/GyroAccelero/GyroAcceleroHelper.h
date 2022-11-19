dc8bb1a6504325088f6fff82274e2a36c45bd740
Sabuj-Ubuntu <sabujbiswasju@gmail.com>
2022-11-20 00:39:44 +0530

    updated

--- src/GyroAccelero/GyroAcceleroHelper.h
+++ src/GyroAccelero/GyroAcceleroHelper.h
@@ -4,126 +4,81 @@
 #include "config.h"
 #include "./Interfaces/BridgeInterface.h"
 
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include "GY521.h"
 
-Adafruit_MPU6050 mpu;
+GY521 sensor(0x69);
+
+uint32_t counter = 0;
 
 void testSetup()
 {
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    Serial.print("Accelerometer range set to: ");
-    switch (mpu.getAccelerometerRange())
-    {
-    case MPU6050_RANGE_2_G:
-        Serial.println("+-2G");
-        break;
-    case MPU6050_RANGE_4_G:
-        Serial.println("+-4G");
-        break;
-    case MPU6050_RANGE_8_G:
-        Serial.println("+-8G");
-        break;
-    case MPU6050_RANGE_16_G:
-        Serial.println("+-16G");
-        break;
-    }
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    Serial.print("Gyro range set to: ");
-    switch (mpu.getGyroRange())
-    {
-    case MPU6050_RANGE_250_DEG:
-        Serial.println("+- 250 deg/s");
-        break;
-    case MPU6050_RANGE_500_DEG:
-        Serial.println("+- 500 deg/s");
-        break;
-    case MPU6050_RANGE_1000_DEG:
-        Serial.println("+- 1000 deg/s");
-        break;
-    case MPU6050_RANGE_2000_DEG:
-        Serial.println("+- 2000 deg/s");
-        break;
-    }
+    Wire.begin();
 
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    Serial.print("Filter bandwidth set to: ");
-    switch (mpu.getFilterBandwidth())
-    {
-    case MPU6050_BAND_260_HZ:
-        Serial.println("260 Hz");
-        break;
-    case MPU6050_BAND_184_HZ:
-        Serial.println("184 Hz");
-        break;
-    case MPU6050_BAND_94_HZ:
-        Serial.println("94 Hz");
-        break;
-    case MPU6050_BAND_44_HZ:
-        Serial.println("44 Hz");
-        break;
-    case MPU6050_BAND_21_HZ:
-        Serial.println("21 Hz");
-        break;
-    case MPU6050_BAND_10_HZ:
-        Serial.println("10 Hz");
-        break;
-    case MPU6050_BAND_5_HZ:
-        Serial.println("5 Hz");
-        break;
-    }
-
-    Serial.println("");
     delay(100);
+    while (sensor.wakeup() == false)
+    {
+        Serial.print(millis());
+        Serial.println("\tCould not connect to GY521");
+        delay(1000);
+    }
+    sensor.setAccelSensitivity(0); // 2g
+    sensor.setGyroSensitivity(0);  // 250 degrees/s
+
+    sensor.setThrottle();
+    Serial.println("start...");
+
+    // set calibration values from calibration sketch.
+    sensor.axe = 0;
+    sensor.aye = 0;
+    sensor.aze = 0;
+    sensor.gxe = 0;
+    sensor.gye = 0;
+    sensor.gze = 0;
 }
 
 void gyroAcceleroSetup()
 {
     MN_DEBUGLN("GyroAccelero Setup");
 
-    if (!mpu.begin())
-    {
-        Serial.println("Failed to find MPU6050 chip");
-        while (1)
-        {
-            delay(100);
-        }
-    }
-    Serial.println("MPU6050 Found!");
-
     testSetup();
 }
 
 void testLoop()
 {
-    /* Get new sensor events with the readings */
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+    sensor.read();
+    int ax = sensor.getAccelX();
+    int ay = sensor.getAccelY();
+    int az = sensor.getAccelZ();
+    int gx = sensor.getGyroX();
+    int gy = sensor.getGyroY();
+    int gz = sensor.getGyroZ();
+    int t = sensor.getTemperature();
 
-    /* Print out the values */
-    Serial.print("Acceleration X: ");
-    Serial.print(a.acceleration.x);
-    Serial.print(", Y: ");
-    Serial.print(a.acceleration.y);
-    Serial.print(", Z: ");
-    Serial.print(a.acceleration.z);
-    Serial.println(" m/s^2");
+    if (counter % 10 == 0)
+    {
+        Serial.println("\n\tACCELEROMETER\t\tGYROSCOPE\t\tTEMPERATURE");
+        Serial.println("\tax\tay\taz\tgx\tgy\tgz\tT");
+    }
 
-    Serial.print("Rotation X: ");
-    Serial.print(g.gyro.x);
-    Serial.print(", Y: ");
-    Serial.print(g.gyro.y);
-    Serial.print(", Z: ");
-    Serial.print(g.gyro.z);
-    Serial.println(" rad/s");
+    Serial.print(counter);
+    Serial.print('\t');
+    Serial.print(ax);
+    Serial.print('\t');
+    Serial.print(ay);
+    Serial.print('\t');
+    Serial.print(az);
+    Serial.print('\t');
+    Serial.print(gx);
+    Serial.print('\t');
+    Serial.print(gy);
+    Serial.print('\t');
+    Serial.print(gz);
+    Serial.print('\t');
+    Serial.print(t);
+    Serial.println();
 
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degC");
-
-    Serial.println("");
-    delay(500);
+    counter++;
+    delay(1000);
 }
 
 void gyroAcceleroLoop()
